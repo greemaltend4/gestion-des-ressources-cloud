e242b92c28c722c505cffd9e6cf446d5028f4d2f
greemaltend4 <dengestabiy@hotmail.com>
2025-08-28 20:29:25 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Gestion des Ressources Cloud</h1>
+      <p>Bienvenue dans l'outil de gestion des ressources AWS.</p>
+    </div>
+  );
+}
+
+export default App;
